66004cdc106a59f6670f4c3cf142cfaada2739f1
zaki <zakiabdillahalkhanif27@gmail.com>
2025-01-10 19:40:35 +0700

    tambah dikit dikit di bagian navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,15 @@
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="container">
+      <div className="flex justify-between">
+        <img src="" alt="" />
+        <ul className="flex gap-4">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Service</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
